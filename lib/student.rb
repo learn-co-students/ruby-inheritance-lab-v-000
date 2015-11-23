b625a7f707db73a53260ee05548649f647a66aa4
Justin Thomas <jthomas0209@gmail.com>
2015-11-23 11:08:51 -0500

    Defined methods/variables for User, Student, and Teacher classes

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+class Student < User
+
+  attr_accessor :knowledge
+
+  def initialize
+    @knowledge =[]
+  end
+
+  def learn(string_info)
+    @knowledge << string_info    
+  end
 
 end
