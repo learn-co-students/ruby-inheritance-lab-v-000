b16164c4499cfb34a9ba6d8dd1164bbef3ce28a2
Tracy <tracetrace@gmail.com>
2015-12-03 22:33:43 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+class Student < User
+
+  attr_accessor :knowledge
+  
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(knowledge_string)
+    @knowledge.push(knowledge_string)
+  end
 
 end
