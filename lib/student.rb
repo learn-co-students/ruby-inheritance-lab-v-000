54b118c363f884d87d4fb9ad8fe30960e9a262bb
tuckerbohman5 <tucker.bohman@gmail.com>
2015-12-04 21:18:53 -0700

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,22 @@
-class Student 
+require_relative 'user.rb'
+
+
+class Student < User
+
+  def initialize
+    @knowledge = []
+  end 
+
+
+  def knowledge 
+    @knowledge
+  end
+
+  def learn(teaching)
+    @knowledge << teaching
+  end
+
+
+
 
 end
