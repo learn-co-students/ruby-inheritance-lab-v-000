b2687880015cf9d8e6d230ea7be8f8dfee4fc6c1
authorbeard <msnolan@gmail.com>
2015-12-11 13:54:35 -0500

    all tests passing

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,6 +1,12 @@
-class Teacher
+# require_relative "./user.rb"
+
+class Teacher < User
+  attr_accessor :first_name, :last_name
 
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javacsript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance varaible", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
+  def teach
+    KNOWLEDGE[rand]
+  end
   
 end
