85c295ff331aca5cd8d3c00f8376780838d862dd
rluftw <luxh93@gmail.com>
2016-01-02 21:51:39 -0500

    Done.

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,6 +1,12 @@
-class Teacher
+require_relative "./user.rb"
 
-  KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javacsript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance varaible", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
+class Teacher < User
+ 	attr_accessor :first_name 
+ 	attr_accessor :last_name 
 
-  
+	KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javacsript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance varaible", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
+
+	def teach 
+		KNOWLEDGE[rand(KNOWLEDGE.length)]
+	end
 end
---
+++ lib/user.rb
@@ -0,0 +1,9 @@
+class User
+	attr_accessor :knowledge
+	attr_accessor :first_name
+	attr_accessor :last_name
+
+	def initialize
+		@knowledge = []
+	end
+end
