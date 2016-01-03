5ea5faf197c2d60bd418edc91dc211ab7fe98349
Tsundu <osmentdan@gmail.com>
2016-01-03 17:22:13 -0500

    added subclasses

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+class Student < User 
+
+attr_accessor :knowledge 
+
+ def initialize
+    @knowledge = []
+  
+ 
+ end
+
+
+ def learn(piece)
+
+  @knowledge << piece 
+   
+ end
 
 end
