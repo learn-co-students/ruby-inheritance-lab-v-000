ce65984c693dd2c8ee92f35feaca55cd7baa4c81
phamducv <phamducv@gmail.com>
2016-01-08 18:59:20 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,27 @@
-class Student 
+class Student < User
+        attr_accessor :first_name, :last_name
 
+
+
+        def initialize
+                @knowledge=[]
+
+        end
+
+        def self.first_name(first_name)
+                @first_name=first_name
+        end
+
+        def self.last_name(last_name)
+                @last_name=last_name
+        end
+
+        def learn(smart)
+                @knowledge << smart
+
+        end
+
+        def knowledge
+                @knowledge
+        end
 end
