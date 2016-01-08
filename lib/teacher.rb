d7bcc81249de1176cbe7dbff8775b3e0abf5f7e8
azheng249 <andy.zheng249@gmail.com>
2016-01-08 17:20:37 +0000

    Done.

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,6 +1,12 @@
-class Teacher
+class Teacher < User
 
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javacsript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance varaible", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
+  @@lessons_taught = 0
   
+  def teach
+    @@lessons_taught += 1
+    KNOWLEDGE[@@lessons_taught - 1]
+  end
+
 end
