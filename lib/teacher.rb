3163d1355ea3aabee15c9003907bef3818a2d1f7
kriskanya <kriskanya@gmail.com>
2016-01-20 16:06:16 -0600

    Done.

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,6 +1,20 @@
-class Teacher
+class Teacher < User
+
+  attr_accessor :first_name, :last_name
 
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javacsript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance varaible", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
+  def initialize
+    @first_name = "Avi"
+    @last_name = "Flombaum"
+  end
+
+  def teach=(input)
+    KNOWLEDGE << input
+  end
+
+  def teach
+    KNOWLEDGE[rand(0..KNOWLEDGE.length-1)]
+  end
   
 end
