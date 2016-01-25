8093e7f185441f12733a25a75d0d484448f85458
franco84 <franco84@gmail.com>
2016-01-25 03:23:31 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
-class Student 
+require "user.rb"
+
+class Student < User
+
+  attr_accessor :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(str)
+    @knowledge << str
+  end
+
+  def knowledge
+    @knowledge
+  end
 
 end
