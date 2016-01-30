ccc3b1ff4db789cc6c42e51595a16904eb450216
michaelsimon <michael.simon@gmail.com>
2016-01-30 14:12:45 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+require './lib/user.rb'
+
+class Student  < User
+
+  attr_accessor :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(string)
+    @knowledge << string
+  end
+
 
 end
