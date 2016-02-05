bb3e392184980c071c23767ae4c0e8157c0a8f7a
zachnewburgh <zach.newburgh@gmail.com>
2016-02-05 15:26:18 -0500

    student, teacher < user

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
 
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(string_of_knowledge)
+    @knowledge << string_of_knowledge
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
