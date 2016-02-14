69022f0397fd9303269bd931be5d382df8f3c5b9
Matthew <mattpreiser@gmail.com>
2016-02-14 04:32:00 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(info)
+        @knowledge << info
+    end
+
+    def knowledge
+        @knowledge
+    end
 
 end
