00a5d732fa669d8387ef64a1c421e6edc7ddca35
Brian Holland <brian.holland@proatria.com>
2016-02-22 21:11:38 -0600

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+class Student < User
+  def initialize
+    @knowledge = []
+  end
 
+  def learn(tidbit)
+    @knowledge.push(tidbit)
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
