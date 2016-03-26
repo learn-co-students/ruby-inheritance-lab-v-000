6fddae74467b35eea2db56788f140cc8f18cb988
Rami97 <rami-sebastian@web.de>
2016-03-26 14:27:10 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+class Student < User
+  def initialize
+    @knowledge = []
+  end
 
+  def learn(string)
+    @knowledge << string
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
