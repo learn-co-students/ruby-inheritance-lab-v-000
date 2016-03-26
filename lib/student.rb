a73bb7a83ce41ae853b8d092f169ba04c77bbd48
PeteAJ <peter.jacobson@gmail.com>
2016-03-26 13:44:21 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,22 @@
-class Student 
+class Student < User
+
+  attr_accessor :first_name, :last_name
+  def initialize
+  @first_name = first_name
+  @last_name = last_name
+end
+
+  KNOWLEDGE = Array.new
+
+def new 
+
+  end
+
+
+  def learn 
+  end 
+
+def knowledge
+end
 
 end
