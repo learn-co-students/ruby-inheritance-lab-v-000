62f50e88a96bf7a934c8f1a7d79e5c76832b94d7
IshmaelKhalid <ishmaelkhalid@icloud.com>
2016-04-19 11:11:35 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(new_knowledge)
+    @knowledge << new_knowledge
+  end
+
+  def knowledge
+    @knowledge
+  end
 
 end
