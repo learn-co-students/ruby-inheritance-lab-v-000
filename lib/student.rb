0b499ed556510db8f486e06de78f2585993b16fa
ThisIsTyDell <milleb94@gmail.com>
2016-05-01 23:10:48 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,11 +1,13 @@
 class Student < User
 
+  attr_accessor :knowledge
+
   def initialize
     @knowledge = []
   end
 
-  def learn(subject)
-    @knowledge << subject
+  def learn(knowledge)
+    @knowledge << knowledge
   end
 
   def knowledge
