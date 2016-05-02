94743b05490255488b5984e649c3470f4e5e32ea
ThisIsTyDell <milleb94@gmail.com>
2016-05-01 23:08:43 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
 
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(subject)
+    @knowledge << subject
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
