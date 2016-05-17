95c4b2c354b9c7c96c856870ac995564d048a163
kaileeagray <kaileeagray@gmail.com>
2016-05-17 16:38:12 -0500

    Done.

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,15 +1,13 @@
 require_relative './user.rb'
-require 'pry'
 
 class Teacher < User
   attr_reader :knowledge
 
-  def knowledge=(knowledge = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javacsript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance varaible", "programming computers hacking learning terminal", "bash Ruby rvm update certs"])
+  def initialize(knowledge = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javacsript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance varaible", "programming computers hacking learning terminal", "bash Ruby rvm update certs"])
     @knowledge = knowledge
   end
 
   def teach
-    binding.pry
     @knowledge.sample
   end
 
