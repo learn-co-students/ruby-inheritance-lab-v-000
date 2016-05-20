531383b2df57889f3b106b3c473087573d126a95
chris m <ninehundredpx@gmail.com>
2016-05-20 18:38:29 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+    attr_accessor
 
+    def initialize
+      @knowledge = []
+    end
+
+    def learn(stuff)
+      @knowledge << stuff
+    end
+
+    def knowledge
+      @knowledge
+    end
 end
