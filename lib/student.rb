aee9616328897f29d39d1be2605707917e9b9a84
rnhutsko <rnhutsko@gmail.com>
2016-05-26 09:44:48 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+class Student < User
+  attr_writer :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn (new_knowledge)
+    @knowledge << new_knowledge
+  end
+
+  def knowledge
+    @knowledge
+  end
 
 end
---
+++ lib/user.rb
@@ -0,0 +1,9 @@
+class User
+  attr_accessor :first_name, :last_name
+
+  def intialize #(first_name,last_name)
+    @first_name #= first_name
+    @last_name #= last_name
+  end
+
+end #of class User  
