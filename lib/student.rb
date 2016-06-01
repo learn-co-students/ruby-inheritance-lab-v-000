c3e336777659d1bb7c69ef6447d092311e70ed73
Dom McKellar <dom.mckellar@gmail.com>
2016-06-01 09:22:56 -0700

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,12 @@
-class Student 
+class Student < User
+  attr_accessor :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(type_of_knowledge)
+    knowledge << type_of_knowledge
+  end
 
 end
