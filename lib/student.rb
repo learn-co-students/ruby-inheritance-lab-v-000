576cd376a33ec1f16b13fbfe200508177e5c7455
Jennifer Dixon <jenniration@gmail.com>
2016-06-21 05:33:47 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
-class Student 
+require_relative "user.rb"
 
+class Student < User
+  attr_accessor :first_name, :last_name
+
+  def initialize
+    @first_name = first_name
+    @last_name = last_name
+    @knowledge = []
+  end
+
+  def learn(info)
+    @knowledge << info
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
