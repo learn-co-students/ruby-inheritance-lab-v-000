b803fb08b0cd6c29259f34db59c05f74418733e0
dmm89 <dmendez89@gmail.com>
2016-07-07 16:59:36 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,21 @@
-class Student 
+
+require'pry'
+class Student < User
+
+  attr_accessor :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+
+  def teach
+    KNOWLEDGE.sample
+  end
+
+  def learn(knowledge)
+    @knowledge << knowledge
+  end
+
+
 
 end
