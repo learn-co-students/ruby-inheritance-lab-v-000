0edf13a64100ef506db5fa29c4086a3dda31c952
Perla Yates <pianistps@yahoo.com>
2016-07-07 19:25:16 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
 
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(string) #adds that stringto students knowledge array
+    @knowledge << string
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
