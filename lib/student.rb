63d751103d0637799eae6274a20ccd5838dba601
Ben Norris <bennorrismisc@gmail.com>
2016-07-08 11:21:40 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,28 @@
-class Student 
+require_relative "./user.rb"
+
+class Student < User
+
+attr_accessor :knowledge, :first_name, :last_name
+
+def initialize
+  @knowledge = []
+end
+
+def first_name
+  @first_name
+end
+
+def last_name
+  @last_name
+end
+
+def learn(subject)
+  @knowledge << subject
+end
+
+def knowledge
+  @knowledge
+end
+
 
 end
---
+++ lib/user.rb
@@ -0,0 +1,9 @@
+class User
+
+attr_accessor :first_name, :last_name
+
+
+
+
+
+end
