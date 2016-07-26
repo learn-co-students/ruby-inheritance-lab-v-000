e1022198385cc95146c0ce5ea1e39d8a5dea1146
Brad Watson <brad.watson.orlando@gmail.com>
2016-07-26 12:56:01 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+class Student < User
+
+  attr_accessor :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(string)
+    @knowledge << string
+  end
+
+  def knowledge
+    @knowledge
+  end
 
 end
