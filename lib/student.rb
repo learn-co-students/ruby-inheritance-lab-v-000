7092eba1d1dde9cf3ab577ff79a6b0923cac7e99
Elyse Klova <elyse.klova@gmail.com>
2016-07-30 22:58:20 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,23 @@
-class Student 
+require_relative "./user.rb"
+
+class Student < User
+  @@all_students = []
+
+  def self.all
+    @@all_students
+  end
+
+  def initialize
+    @knowledge = []
+    @@all_students << self
+  end
+
+  def knowledge
+    @knowledge
+  end
+
+  def learn(string)
+    @knowledge << string
+  end
 
 end
