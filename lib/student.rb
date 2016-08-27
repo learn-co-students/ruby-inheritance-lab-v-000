5d8937053c79f294ba5c845b9613251a10e2420e
jack woodward <jackwoodwardschool@gmail.com>
2016-08-27 23:19:49 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require_relative "./user.rb"
+
+class Student < User
+attr_accessor :teacher
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(lesson)
+        @knowledge << lesson
+    end
+
+    def knowledge
+        @knowledge
+    end
 
 end
