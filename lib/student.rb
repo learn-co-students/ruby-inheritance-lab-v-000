7c5769a023fb9bbaade08684d3b5fc10488fd8ea
Will Lynch <will.lynch91@gmail.com>
2016-09-18 15:02:21 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,12 @@
-class Student 
+class Student < User
+  attr_reader :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(knowledge)
+    self.knowledge << knowledge
+  end
 
 end
