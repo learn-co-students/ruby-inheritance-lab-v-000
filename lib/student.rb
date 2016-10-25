40f35b5e7c9560e8144f717bcf8f571a35439da5
Jesse Wenger <wengerj@gmail.com>
2016-10-25 16:33:38 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
 
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(str_knowledge)
+    @knowledge << str_knowledge
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
