1659e1053a98c2a96306a7f90a3d44e317775b6f
Jon Fougner <jon.fougner@gmail.com>
2016-10-26 23:51:18 +0000

    Done.

---
+++ lib/user.rb
@@ -0,0 +1,19 @@
+class User
+
+  def first_name=(first_name)
+    @first_name = first_name
+  end
+
+  def first_name
+    @first_name
+  end
+
+  def last_name=(last_name)
+    @last_name = last_name
+  end
+
+  def last_name
+    @last_name
+  end
+
+end
