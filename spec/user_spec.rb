c71c5628c6e496be472005025082c65820d7bd79
Bruno Boehm <bruno.boehm@gmail.com>
2016-10-28 05:59:19 +0000

    Done.

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,13 +1,13 @@
 require "spec_helper"
 
-describe "User" do 
+describe "User" do
   let!(:user) { User.new }
-  it "has a first name" do 
+  it "has a first name" do
     user.first_name = "Tracy"
     expect(user.first_name).to eq("Tracy")
   end
 
-  it "has a last name" do 
+  it "has a last name" do
     user.last_name = "Morgan"
     expect(user.last_name).to eq("Morgan")
   end
