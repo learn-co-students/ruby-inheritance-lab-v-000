301bbdca5a24518fa7c149a8c89a838eb4cfe610
Mahdi Shadkamfarrokhi <mahdi@allstarcode.org>
2016-10-28 15:19:38 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,12 @@
-class Student 
+class Student < User
+    attr_accessor :knowledge # :first_name, :last_name, 
+
+    def initialize
+        @knowledge =[]
+    end
+
+    def learn(know)
+        @knowledge << know
+    end
 
 end
---
+++ lib/user.rb
@@ -0,0 +1,6 @@
+class User
+    attr_accessor :first_name, :last_name
+
+    @first_name = "Trace"
+    @last_name = "Morgan"
+end
