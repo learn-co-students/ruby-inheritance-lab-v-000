9dcf3eb1770f342c5b24fbe2b3d1c3a5d2e1dc7d
Alexis Roman <alexismmroman21@gmail.com>
2016-12-30 04:19:00 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,21 @@
-class Student 
+require_relative "./user.rb"
+
+class Student < User
+
+  def initialize
+
+    @knowledge = []
+
+  end
+
+  def learn(knowledge)
+    @knowledge << knowledge
+
+  end
+
+  def knowledge
+    @knowledge
+
+  end
 
 end
