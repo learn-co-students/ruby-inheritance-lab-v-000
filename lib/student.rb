700a27f23cfbcfd9746ee7aba5df8473db978620
Kevin Adkins <kevinladkins@gmail.com>
2017-01-29 18:32:53 +0000

    built Teacher #teach method

--- lib/student.rb
+++ lib/student.rb
@@ -2,4 +2,16 @@ require_relative "./user.rb"
 
 class Student < User
 
+  def initialize
+    @knowledge = []
+  end
+
+  def learn
+    @knowledge << Teacher.teach
+  end
+
+  def knowledge
+    @knowledge
+  end
+
 end
