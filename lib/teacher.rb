51e0d575c8077ae95c7e76c98c36a2f72efdff98
Daniel Lempesis <danlrcn@hotmail.com>
2017-02-21 23:43:08 +0000

    Done.

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -3,7 +3,7 @@ class Teacher < User
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
   def teach
-    random_quote_index = rand(0..KNOWLEDGE.length)
+    random_quote_index = rand(0..KNOWLEDGE.length)  #random.sample is the default apparently
     KNOWLEDGE[random_quote_index]
     end
 
