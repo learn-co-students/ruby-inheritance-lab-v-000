b8d3ac836cc1b88c90e7a13ea0e329e9a6fa7720
Jingru Zhang <zhang.jingru.93@gmail.com>
2017-03-14 22:04:07 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+class Student < User
+  def initialize
+    @knowledge = []
+  end
 
+  def learn(content)
+    @knowledge << content
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
