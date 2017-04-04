a395c837e2ae6eebdcb9ab4247a802b488d0c73a
William Barela <william.barela.dev@gmail.com>
2017-04-04 06:25:50 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+require_relative "./user"
 
+class Student < User
+  attr_accessor :knowledge
+  
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(stringy_knowledge)
+      self.knowledge << stringy_knowledge
+  end
+  
 end
