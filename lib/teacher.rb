4d7a30ea3085997f0712aa523a4dcaa43cacc553
Eva Navarror <emanao@me.com>
2017-04-14 19:20:48 +0000

    Done.

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,10 +1,16 @@
 class Teacher < User
-
+  attr_reader :students
   @@KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
-  def knowledge_reader
+  def initialize
+    @students = []
+  end
+  def knowledge
     @@KNOWLEDGE
   end
+  def add_student(student)
+    self.students<<student
+  end
   def teach
-    self.knowledge_reader.sample
+    self.knowledge.sample
   end
 end
