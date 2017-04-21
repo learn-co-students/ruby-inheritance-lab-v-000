8c57b1e05ca2d5d5258af06911b836bc77712465
Joseph Hyatt <jhyatt1017@gmail.com>
2017-04-21 16:35:52 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,28 @@
-class Student 
+#####inherits from the User class#####
+#####first_name
+#has a first name
+#####last_name
+#has a last name
+#####new
+#initializes with an empty knowledge array
+#####learn
+#takes in an argument of a string of knowledge and adds it to the student's knowledge array
+#####knowledge
+#returns that student's knowledge array
 
+require_relative "./user.rb"
+
+class Student < User
+
+  def initialize()
+    @knowledge = []
+  end
+
+  def learn(knowledge)
+    @knowledge << knowledge
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
