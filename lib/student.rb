8e720ed027514bc1c6ba8acbc9defd8dc46395d7
lucketta <lucketta@umich.edu>
2017-04-22 21:25:32 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+require_relative "user.rb"
+
+class Student < User
+  attr_reader :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(str)
+    self.knowledge << str
+  end
 
 end
