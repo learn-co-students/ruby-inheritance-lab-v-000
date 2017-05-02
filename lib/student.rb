f6739bb70a4bb80698c348712df27e19983759b3
Will-Sauer <will@heueso.com>
2017-05-02 07:21:12 -0700

    student stuff working

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,26 @@
-class Student 
+require_relative './user.rb'
+
+class Student < User
+#----------------------------------------------------------------------------------------
+#vars and macros
+
+
+#----------------------------------------------------------------------------------------
+#instance
+def initialize
+@knowledge = []
+end
+
+def learn(new_knowledge)
+@knowledge << new_knowledge
+end
+
+def knowledge
+@knowledge
+end
+
+#----------------------------------------------------------------------------------------
+#class
+
 
 end
