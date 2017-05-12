a70b855397aa42ad724aa3e9a2045b2fbea4d824
curlywallst <nnoyes@comcast.net>
2017-05-12 12:49:39 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
-class Student 
+require_relative './user.rb'
+
+class Student < User
+  attr_accessor :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(fact)
+    @knowledge << fact
+  end
+
+  def knowledge
+    @knowledge
+  end
+
 
 end
