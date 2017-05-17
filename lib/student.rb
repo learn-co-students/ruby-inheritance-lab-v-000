30431dba9846f07f07fd6d3f65695c776da5b1fe
hal_fields <hfields697@aol.com>
2017-05-17 10:56:31 -0500

    done

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student  < User
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(bits_of_knowledge)
+        @knowledge << bits_of_knowledge
+    end
+
+    def knowledge
+        @knowledge
+    end
 
 end
