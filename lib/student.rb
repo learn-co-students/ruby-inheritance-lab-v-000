3f585159747846a419ec00a8cfcedac22d21c46d
Lara Matyushenko <larabrightside@gmail.com>
2017-05-17 18:10:36 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+require_relative "./user.rb"
+
+class Student < User
+
+  attr_reader :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(knowledge)
+    @knowledge << knowledge
+  end
 
 end
