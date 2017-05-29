7d02f3198ad4bfe6c227a3785ea53cd1544421be
Derrick Chu <dc79627n@pace.edu>
2017-05-29 13:41:43 +0000

    Done.

---
+++ lib/user.rb
@@ -0,0 +1,9 @@
+#define super class user so teacher and student can inherit from User
+
+class User
+
+  #user has first and last name(setter and getter)
+
+  attr_accessor :first_name, :last_name
+
+end
