489615cbed6a3c34be01b7663bb12aedd4abb403
Jacilyn Hayden <jacihayden@gmail.com>
2017-06-03 23:06:02 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
-class Student 
+require_relative "./user.rb"
 
+class Student < User
+
+  def initialize
+    @first_name = first_name
+    @last_name = last_name
+    @knowledge = []
+  end
+
+
+  def learn(knowledge)
+    @knowledge << "#{knowledge}"
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
