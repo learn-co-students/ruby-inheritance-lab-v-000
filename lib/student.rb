81437a9d29ad33d328358351eed7a9d17a9e6cbd
github username <github email address>
2017-06-06 16:15:36 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,24 @@
-class Student 
+#1) Student inherits from the User class
+     #Failure/Error: expect(Student.ancestors).to include(User)
+require_relative "./user.rb"
+#Student #new initializes with an empty knowledge array
+     #Failure/Error: expect(sophie.instance_variable_get(:@knowledge)).to eq([])
+
+class Student < User
+  attr_accessor :first_name, :last_name
+
+  def initialize
+    @knowledge = []
+  end
+
+  #1) Student #learn takes in an argument of a string of knowledge and adds it to the student's knowledge array
+     #Failure/Error: student.learn("Ruby framework Rails gem bundle update")
+  def learn(knowledge)
+    @knowledge << knowledge
+  end
+
+  def knowledge
+    @knowledge
+  end
 
 end
