eb1c4498b38194f9025d636054e311c9b671310b
Nicholas D'Amico <nickalan82@icloud.com>
2017-06-16 13:09:49 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+class Student < User
+
+  attr_writer :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(knowledge)
+    self.knowledge << knowledge
+  end
+
+  def knowledge
+    @knowledge
+  end
 
 end
