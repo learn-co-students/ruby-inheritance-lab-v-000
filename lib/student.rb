24b76945b17cad4e29ed94990593cff1a6ec24d3
kyleblee <hello.kylelee@gmail.com>
2017-07-01 16:13:09 -0400

    lab complete

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+require_relative './user.rb'
 
+class Student < User
+  attr_reader :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(new_knowledge)
+    @knowledge << new_knowledge
+  end
 end
