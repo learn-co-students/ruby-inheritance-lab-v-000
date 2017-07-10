58acbab0f79a865840b0b0688bf0e8665e3a4454
Robert Peckham <peckham.rob@gmail.com>
2017-07-10 13:55:00 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+#require "./user.rb"
+
+class Student < User
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(knowledge)
+    @knowledge << knowledge
+  end
+
+  def knowledge
+    @knowledge
+  end
+
 
 end
