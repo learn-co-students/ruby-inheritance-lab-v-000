11a6d9a98a69de3afbfce845a22d983ca4f4e79a
Enoch2k2 <enoch2k2@hotmail.com>
2017-07-21 11:55:56 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,11 @@
-class Student 
+class Student < User
+  attr_accessor :knowledge
 
+  def initialize
+    self.knowledge = []
+  end
+
+  def learn(topic)
+    self.knowledge.push(topic)
+  end
 end
