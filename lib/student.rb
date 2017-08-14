6b686f76394941c6d68e549fdf0212ab80f51ad3
Marcelo A Carrillo <marceloacarrillo@gmail.com>
2017-08-14 02:09:24 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+
+  def initialize
+    @knowledge = Array.new
+  end
+
+  def learn(str)
+    @knowledge << str
+  end
+
+  def knowledge
+    @knowledge
+  end
 
 end
