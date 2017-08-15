d86312342b4c68a9353e912d2da683dca2737c26
Erin Land <erinland05@gmail.com>
2017-08-15 01:43:29 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+require_relative "./user.rb"
 
+class Student < User
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(string)
+    @knowledge.push(string)
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
