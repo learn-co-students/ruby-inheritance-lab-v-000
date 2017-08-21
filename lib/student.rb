5e0508872fe890542294a098940b9e00d46d1c73
Archana Burra <burra.archana@gmail.com>
2017-08-21 18:40:05 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+class Student < User
+  attr_accessor :new
 
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(knowledge)
+    @knowledge << knowledge
+  end
+
+  def knowledge
+    r = Random.new
+    @knowledge[r.rand(0..@knowledge.length-1)]
+  end
 end
