628e2f71db52875788a4aebb1b1f94424f80078c
Mary Benitez <meebenitez@gmail.com>
2017-08-28 04:47:10 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,22 @@
-class Student 
+require 'user.rb'
+
+class Student < User
+
+  attr_accessor :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(string)
+    @knowledge << string
+  end
+
+  def knowledge
+    @knowledge
+  end
+  
+
+
 
 end
