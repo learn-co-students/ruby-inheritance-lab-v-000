2da14e9eb82f17dca5eb451bfb2c6d7ce2b806f2
Chris Rudolph <chrisjames.r@gmail.com>
2017-08-31 00:49:32 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
 
-end
+
+class Student < Teacher
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(string)
+    self.knowledge << string
+  end
+
+  def knowledge
+    @knowledge
+  end
+end  # End of Class
