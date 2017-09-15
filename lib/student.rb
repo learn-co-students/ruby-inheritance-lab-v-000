8af9c6d853f8120f5132baa917cca33aa8046570
Justin Tooley <jstooley823@gmail.com>
2017-09-15 05:50:14 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+require_relative "./user.rb"
+class Student < User
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn (learned)
+    @knowledge << learned
+  end
+
+  def knowledge
+    @knowledge.dup.freeze
+  end
 
 end
