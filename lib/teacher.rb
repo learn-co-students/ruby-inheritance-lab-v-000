941e5f4befef6ae274e287e81de96f7097a0c7ae
Sara Stanton <sara.mpa13@gmail.com>
2017-09-18 04:48:44 +0000

    Done.

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -8,9 +8,4 @@ class Teacher < User
     p "#{KNOWLEDGE[rand(0..7)]}"
   end
 
-  # def initialize(first_name, last_name)
-  #   @first_name = first_name
-  #   @last_name = last_name
-  # end
-
 end
