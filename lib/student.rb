6b2f463b5177c497e24b5ad75bfd7f46d0e54f09
Melissa Amaya <amaya.melissa@gmail.com>
2017-09-20 22:17:53 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+class Student < User
 
+  attr_accessor :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(line)
+    @knowledge << line
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
