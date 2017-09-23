a273d55b1d7836a13ca77658980e5d1d88773c27
Vicky Lau <vlau.nyc@gmail.com>
2017-09-23 04:24:35 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+require_relative "./user.rb"
+
+class Student < User
+  attr_accessor :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(x)
+    @knowledge << x
+  end
 
 end
