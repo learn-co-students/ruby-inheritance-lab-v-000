36989a26414f3d29a285b56a5803034dd3dfc5eb
Sergio Correa <correa.sergio@gmail.com>
2017-09-27 01:57:09 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+class Student < User
+
+  attr_reader :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(knowledge)
+    self.knowledge << knowledge
+  end
 
 end
