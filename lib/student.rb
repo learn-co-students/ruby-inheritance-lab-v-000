404eecbb3854a264d841caee2a43f7a0b2b8c181
Justin Daniel <justinddaniel@gmail.com>
2017-09-28 16:58:28 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+class Student < User
+
+attr_accessor :knowledge
+
+def initialize
+  @knowledge = []
+end
+
+def learn(fact)
+  self.knowledge << fact
+end
+
+def knowledge
+  @knowledge
+end
 
 end
