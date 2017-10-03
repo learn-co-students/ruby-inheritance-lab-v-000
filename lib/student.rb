3b79d36c364e1357f353f03037a2193417c125eb
Purnima  Lakshmi Narayanan <purnima0912@hotmail.com>
2017-10-03 00:00:28 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
 
+attr_accessor :str
+  def initialize
+    @knowledge = []
+  end
+
+  def learn (str)
+    @knowledge << str
+  end
+
+  def knowledge
+    @knowledge 
+  end
 end
