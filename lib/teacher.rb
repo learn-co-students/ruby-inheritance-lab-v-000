2f88ec2d6e8a5109c7262e75d7755ecc4e4e7db4
Miriam Peskowitz <miriam5000@gmail.com>
2017-10-04 14:30:10 +0000

    yay

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -4,6 +4,14 @@ class  Teacher < User
 
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
+  def first_name=(name)
+    @first_name = name
+  end
+
+  def last_name=(name)
+    @last_name = name
+  end
+
   def teach
     KNOWLEDGE.sample
   end
--- lib/user.rb
+++ lib/user.rb
@@ -2,12 +2,9 @@ class User
 
   attr_accessor :first_name, :last_name
 
-  def initialize(first_name, last_name)
-
+  def initialize
+      @first_name = first_name
+      @last_name = last_name
   end
 
-
-  def Jim
-      jim = User.new("Jim Wigs")
-  end
 end
