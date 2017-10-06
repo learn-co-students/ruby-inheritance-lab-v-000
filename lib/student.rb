31489f5156240f65d47a8a10c33707de6c7a3865
Adam Tropp <adt6261@gmail.com>
2017-10-06 16:27:46 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
 
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(information)
+    @knowledge << information
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
