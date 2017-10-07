0d699f1de97b2127b2389ba24f017f687251a1e4
Lisa Lee <lisa_lee@brown.edu>
2017-10-07 15:46:51 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require_relative "./user.rb"
+class Student < User
+  attr_accessor :knowledge
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(knowledge)
+    @knowledge << knowledge
+  end
+
+  def knowledge
+    @knowledge
+  end
+
 
 end
