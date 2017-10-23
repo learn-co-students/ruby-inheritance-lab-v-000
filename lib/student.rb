5d6b4bd0186b1e53b94eb91c10dd863fe6bbea25
Julianne Kim <juliannne@gmail.com>
2017-10-23 21:51:49 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,11 +1,15 @@
 class Student < User
   attr_accessor :knowledge
-  
+
   def initialize
     @knowledge = []
   end
 
   def learn(string)
     knowledge << string
+  end
+
+  def knowledge
+    @knowledge
   end
 end
