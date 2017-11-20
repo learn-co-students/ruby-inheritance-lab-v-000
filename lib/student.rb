c0e71147270a5cac7998fefba5d2a6d991f311a8
Rachel Miller <raeanne1991@gmail.com>
2017-11-20 17:15:31 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+require_relative "./user.rb"
+class Student < User
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(pearl)
+    @knowledge << pearl
+  end
+
+  def knowledge
+    @knowledge
+  end
 
 end
