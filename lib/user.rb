ec7b4184d17fa4f728677074c86c38c41d3d2898
Joshua Needham <joshuabneedham@gmail.com>
2017-11-20 17:34:30 +0000

    Done.

--- lib/user.rb
+++ lib/user.rb
@@ -1,8 +1,16 @@
 class User
   attr_accessor :first_name, :last_name
 
-  def initialize(first_name, last_name)
+  def initialize
     @first_name = first_name
-    @last_name = last_name    
+    @last_name = last_name
+  end
+
+  def first_name
+    @first_name
+  end
+
+  def last_name
+    @last_name
   end
 end
