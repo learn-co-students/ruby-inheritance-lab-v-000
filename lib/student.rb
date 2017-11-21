502e7cebed3c0225da00a47d14b86dd8a9baeed6
jramosch <jamilya_ramos-chapman@alumni.brown.edu>
2017-11-21 03:59:20 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require_relative "./user.rb"
 
+class Student < User
+  attr_accessor :first_name, :last_name
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(string)
+    @knowledge << string
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
