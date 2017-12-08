b2afa44e692456e0d2b11ec95662a3baec1ce9e6
charityyoungblood <youngblood.charity@gmail.com>
2017-12-07 16:31:57 -0800

    added initialize method and placeholder for learn method in student.rb

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,14 @@
 require 'pry'
 
 class Student < User
-
+  # create initialize method - no arguments 
+    # - should have @knowledge array = []
+  def initialize
+    @knowledge = []
+  end 
+  
+  def learn
+    
+  end 
+  
 end
