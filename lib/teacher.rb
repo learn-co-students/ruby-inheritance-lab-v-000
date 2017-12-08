1fb56eed9e802d155f7b8ef086d975eb11949ec8
charityyoungblood <youngblood.charity@gmail.com>
2017-12-07 16:28:34 -0800

    added sample method to return random string

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -8,6 +8,9 @@ class Teacher < User
 
   def teach
     # this method should return a random string from the KNOWLEDGE contant array
+    # how would you select a random portion from array? look up random number
+    KNOWLEDGE.sample
+      
   end 
   
 end
