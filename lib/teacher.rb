78eb6d30631c29c90cab00dcd4dbc82a14b57b80
Anthony Harvey <anthonygharvey@gmail.com>
2017-12-17 21:46:20 +0000

    Done.

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,6 +1,20 @@
-class Teacher
+class Teacher < User
 
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
-  
+  def teach
+    random_number = rand(1..KNOWLEDGE.count)
+    KNOWLEDGE[random_number]
+  end
+
+  def all_student_knowledge
+    Student.students.each do |student|
+      puts "#{student.first_name} knows:"
+      student.knowledge.each do |item|
+        puts "#{item}."
+      end
+      puts
+    end
+  end
+
 end
