dfaa5e07adbcf648457e0969ed2654e0ed0e0b97
Amanda White <emmalinerosewhite@gmail.com>
2018-01-18 21:13:09 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require_relative "./user.rb"
 
+class Student < User
+  attr_accessor :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(new_info)
+    @knowledge << new_info
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
