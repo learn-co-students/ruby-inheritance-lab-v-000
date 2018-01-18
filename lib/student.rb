62a424801f035ad0bf724a23d701df3ef6333f1e
Cam Nguyen <cnguyen1187@gmail.com>
2018-01-18 21:28:26 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+class Student < Teacher
+  def initialize
+     @knowledge = []
+   end
 
+   def learn(string)
+     self.knowledge << string
+   end
+
+   def knowledge
+     @knowledge
+   end
 end
