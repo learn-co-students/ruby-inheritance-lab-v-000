eb164d3711af704d5e98d732d5fdca96ba6c2ce5
Sean Clifford <sclifford6262@gmail.com>
2018-01-23 03:14:11 +0000

    Done.

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,6 +1,12 @@
-class Teacher
+#require "lib/user.rb"
+
+class Teacher < User
 
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
-  
+  def teach
+    arr_size = KNOWLEDGE.length
+    KNOWLEDGE[rand(0...arr_size - 1)]
+  end
+
 end
