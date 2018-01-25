38b2e57754825a72b4e15d52711363abac685327
Jul Jen-Prog <juliajen.prog@gmail.com>
2018-01-25 20:44:41 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+require_relative "./teacher.rb"
+
+class Student <  User
+
+  attr_accessor :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(know)
+    @knowledge << know
+  end
 
 end
