de3c27a8fd5f498277dee8a31f09d2837dbdb15b
EPIC448 <samrey2018@gmail.com>
2018-01-28 01:29:33 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,20 @@
-class Student 
+require 'pry'
+require_relative "./user.rb"
+class Student < User
 
+  def initialize
+    # binding.pry
+      @knowledge = []
+
+  end
+
+  def learn(knowledge)
+      @knowledge << knowledge
+
+  end
+
+  def knowledge
+    @knowledge
+
+  end
 end
