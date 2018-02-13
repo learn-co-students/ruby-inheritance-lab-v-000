653abf663d4c9b2ea6a3307af7b3c6947efa718c
Chely Ho <adept_inept@yahoo.com>
2018-02-13 16:13:09 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,12 @@
-class Student 
+class Student < User
+  attr_accessor :knowledge
+
+  def initialize              # students start with no knowledge. LOL!
+    @knowledge = []
+  end
+
+  def learn(info)
+    self.knowledge << info
+  end
 
 end
