7556726e038bbf755ed749480f02cb601e5e77a3
Chris Henry <cdhenry@gmail.com>
2018-02-17 19:02:13 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require_relative "./user.rb"
+
+class Student < User
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(topic)
+    @knowledge << topic
+  end
+
+  def knowledge
+    @knowledge
+  end
 
 end
