056244428a71b2a6f6e407aa40b29abc98d8f5e9
Jay Bednar <ironviralmedia@gmail.com>
2018-02-19 18:31:00 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+class Student < User
+  attr_accessor = :knowledge
 
+  def initialize
+    @knowledge = []
+  end
+
+  def knowledge
+    @knowledge
+  end
+
+  def learn(subject)
+    self.knowledge << subject
+  end
+  
 end
