24cf0ea126d224ae8c1e17c54563cde8e1e1bc6f
Jayon Kim <jpkim921@gmail.com>
2018-03-03 20:01:05 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,11 @@
-class Student 
+class Student < User
+  attr_accessor :knowledge
 
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(string_of_knowledge)
+    self.knowledge << string_of_knowledge
+  end
 end
