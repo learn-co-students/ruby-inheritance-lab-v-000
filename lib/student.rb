da50d13a711ae5e9fe7854de4e022c58d61d0a5a
Lindsey Trainor <ltrainor38@gmail.com>
2018-03-05 03:03:35 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
-class Student 
+require_relative "./user.rb"
 
+class Student < User 
+  
+  attr_accessor 
+  
+  def initialize()
+    @knowledge = []
+  end 
+  
+  def learn(k)
+    @knowledge << k 
+  end 
+
+  def knowledge 
+    @knowledge 
+  end 
+  
 end
