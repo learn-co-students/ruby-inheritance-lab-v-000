7dd1227ddd86b85804d6ef602c4a73960139cb1c
Brandon Brasson <rocksong622@gmail.com>
2018-03-15 19:56:28 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,10 @@
-class Student 
+class Student
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @name= []
+  end
 
 end
---
+++ lib/user.rb
@@ -0,0 +1,6 @@
+class User
+attr_accessor :first_name,last_name:
+  def initialize(first_name)
+    @first_name = first_name
+    @last_name = []
+  end
