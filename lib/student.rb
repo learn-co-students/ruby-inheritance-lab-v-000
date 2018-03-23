a1f2aaffb7f777d786e21f6ea34982cbbde68e8f
Katie Cassell <kcassell944@gmail.com>
2018-03-23 22:25:46 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,18 @@
-class Student
+class Student < User
   attr_accessor :first_name, :last_name
 
   def initialize
     @first_name = first_name
     @last_name = last_name
+    @knowledge = []
+  end
+
+  def learn(knowledge)
+    @knowledge << knowledge
+  end
+
+  def knowledge
+    @knowledge
   end
 
 
