3aba07aebe4bc68713573d74f8c45b1c184bbbec
cam <camsay@gmail.com>
2018-03-30 15:16:30 -0700

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+class Student < User
+  attr_accessor :first_name, :last_name
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(string)
+    @knowledge << string
+  end
+
+  def knowledge
+    @knowledge
+  end
 
 end
--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,6 +1,15 @@
-class Teacher
+class Teacher < User
+  attr_accessor :first_name, :last_name
+
+  def initialize
+    @first_name
+    @last_name
+  end
 
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
-  
+  def teach
+    KNOWLEDGE[rand]
+  end
+
 end
