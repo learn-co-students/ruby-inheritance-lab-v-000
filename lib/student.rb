c91df25f3ac98af91e20a8b0358341c7338fff2d
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-04-11 22:50:24 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,20 @@
-class Student 
+require_relative "./user.rb"
+
+class Student < User
+  def initialize
+    @knowledge = []
+  end
+
+
+  def new
+  end
+
+  def learn(info)
+    @knowledge << info
+  end
+
+  def knowledge
+    @knowledge
+  end
 
 end
