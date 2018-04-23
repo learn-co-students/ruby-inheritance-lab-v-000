36f75eb609eefaa248a9784ee4b7a1c89679da07
Andrey Syroyezhkin <asyroyez@gmail.com>
2018-04-23 09:29:30 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,12 @@
-class Student 
+class Student < User
 
+  attr_accessor :knowledge
+
+  def initialize
+    @knowledge = Array.new
+  end
+
+  def learn(knowledge_string)
+    @knowledge << knowledge_string
+  end
 end
