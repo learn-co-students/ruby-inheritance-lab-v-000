64db89eee988308fab8cb641955be0c273bdb58e
Karen G <kgraves0701@gmail.com>
2018-04-24 22:03:04 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+class Student < User
+
+  attr_accessor :knowledge
+
+  def initialize(knowledge = [])
+    @knowledge = knowledge
+  end
+
+  def learn(string)
+    @knowledge << string
+  end
+
+  def knowledge
+    @knowledge
+  end
 
 end
