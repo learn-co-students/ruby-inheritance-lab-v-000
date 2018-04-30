3e5470c078baa83e8a06c0a8744f2d44d802a7e9
Marcus  = Soulo <souloinc87@gmail.com>
2018-04-30 18:17:24 +0000

    Done.

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,6 +1,10 @@
-class Teacher
+class Teacher < User
+
 
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
-  
+  def teach
+    random_element = KNOWLEDGE
+    random_element.sample
+  end
 end
