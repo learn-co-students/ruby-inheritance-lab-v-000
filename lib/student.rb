11dc25b8a4efed9f5521ff2128a6b1c14cb64585
Trisha Garrett <trisha.garrett@gmail.com>
2018-05-08 19:17:11 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+require_relative './user.rb'
 
+class Student < User
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(string_of_knowldege)
+    @knowledge << string_of_knowldege
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
