85f94a262533bef215e75b5d3b78c63a1e38d1b8
Daksha Patel <dakshapatel100@gmail.com>
2018-05-16 13:29:54 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+class Student < User
+
+attr_accessor :knowledge_array
+
+
+def initialize
+  @knowledge = []
+end
+
+def learn(knowledge)
+  @knowledge << knowledge
+end
+
+def knowledge
+  @knowledge
+end
 
 end
