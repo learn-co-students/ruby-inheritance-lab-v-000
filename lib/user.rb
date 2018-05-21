f5689ba238023e93de3ffd6163271b4126252719
Amar Chatman <amarjerome@gmail.com>
2018-05-21 19:29:42 +0000

    Done.

---
+++ lib/user.rb
@@ -0,0 +1,18 @@
+class User
+
+attr_accessor :first_name, :last_name
+
+def initialize
+  @knowledge = []
+end
+
+def learn(knowledge)
+  @knowledge << knowledge
+end
+
+def knowledge
+  @knowledge
+end 
+
+
+end
