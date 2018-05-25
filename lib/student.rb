1ea1343823c596cc0f0be9b6390677395c6c067a
taryn yaeger <tsyaeger@gmail.com>
2018-05-25 20:00:52 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,20 @@
-class Student 
+
+
+
+class Student < User
+  
+  attr_accessor = []
+  
+  def initialize
+    @knowledge = []
+  end
+  
+  def learn(know_str)
+    @knowledge << know_str
+  end 
+  
+  def knowledge
+    @knowledge
+  end
 
 end
