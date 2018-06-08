00ac7c928230d8910facdecc1d8d5279a1a4c302
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-06-08 21:22:55 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,5 @@
 class Student < User
-  
+
   attr_accessor :knowledge
 
   def initialize
@@ -8,6 +8,10 @@ class Student < User
 
   def learn(knowledge)
     @knowledge << knowledge
+  end
+
+  def knowledge
+    @knowledge
   end
 
 end
