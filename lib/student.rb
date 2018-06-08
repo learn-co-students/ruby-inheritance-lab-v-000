b28044de0c52d93ad5c4706daa65b5704fa49ec1
Joey Cho <cjy12182000@naver.com>
2018-06-08 11:51:43 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(string)
+    self.knowledge << string
+  end
+
+  def knowledge
+    @knowledge
+  end
 
 end
