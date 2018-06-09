48c80cb1993495095b4dda8280cff20bd060d008
Natalia Castro <nat.alvz@gmail.com>
2018-06-09 21:11:45 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+require_relative "./user.rb"
+class Student < User
+  attr_accessor :knowledge
+  
+ def initialize
+   @knowledge = []
+ end
 
+ 
+ def learn(skill)
+   @knowledge << skill 
+ end 
+ 
+  
+ def knowledge
+   @knowledge
+ end
 end
