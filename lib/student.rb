0666075a4158a0d7f51e60072d767fb2c4950a6c
Michael Blandford <mdblandford@gmail.com>
2018-06-27 15:13:39 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,14 +1,12 @@
 class Student < User
-  
+attr_accessor :knowledge
+
   def initialize
     @knowledge = []
   end
-  
+
   def learn(string)
     @knowledge << string
   end
-  
-  def knowledge
-    @knowledge
-  end
+
 end
