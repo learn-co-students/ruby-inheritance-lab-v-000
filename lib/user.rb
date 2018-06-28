5af9dbede7f7319d8c06744d5eec3fb6ddd79622
Vicki Hamm <vicki.cello@gmail.com>
2018-06-28 21:19:38 +0000

    Done.

---
+++ lib/user.rb
@@ -0,0 +1,16 @@
+class User
+
+  attr_accessor :first_name, :last_name
+  attr_reader :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(string)
+    @knowledge << string
+  end
+
+
+
+end
