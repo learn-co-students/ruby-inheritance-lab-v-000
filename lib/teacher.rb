4d430c947f789fae2b7321cb4acde56daf547088
Leslie Stevens <flylady@mail.utexas.edu>
2018-06-29 21:43:17 +0000

    Done.

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -4,7 +4,7 @@ class Teacher < User
   attr_accessor :first_name, :last_name
 
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
-  binding.pry
+
   def first_name
     @first_name
   end
@@ -14,6 +14,6 @@ class Teacher < User
   end
 
   def teach
-    KNOWLEDGE.inspect
+    KNOWLEDGE.sample
   end
 end
