54891978a2cb9e439b4dbbb573a9d65de3c3e815
Leslie Stevens <flylady@mail.utexas.edu>
2018-06-29 21:28:29 +0000

    Done.

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,7 +1,19 @@
+require 'pry'
 require_relative "./user.rb"
 class Teacher < User
+  attr_accessor :first_name, :last_name
 
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
+  binding.pry
+  def first_name
+    @first_name
+  end
 
+  def last_name
+    @last_name
+  end
 
+  def teach
+    KNOWLEDGE.inspect
+  end
 end
--- lib/user.rb
+++ lib/user.rb
@@ -1,2 +1,3 @@
 class User
+  attr_accessor :first_name, :last_name
 end
