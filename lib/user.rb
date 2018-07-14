d1f0b2667443a48b4305cc7ee0855e6bb2476d59
nathalie <nathalie.scruz@gmail.com>
2018-07-14 19:46:53 +0000

    Done.

--- lib/user.rb
+++ lib/user.rb
@@ -1,9 +1,6 @@
+
 class User
 
-  attr_accessor :first_name
-
-  def initialize(first_name, last_name)
-    @first_name, @last_name = first_name, last_name
-  end
+  attr_accessor :first_name, :last_name
 
 end
