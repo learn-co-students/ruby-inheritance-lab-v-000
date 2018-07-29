3ed765301275f6fecac4dd17a5a04e86e7b97a94
Justine Bricknell <justinegayton@gmail.com>
2018-07-29 14:22:48 +0000

    Done.

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -3,7 +3,7 @@ class Teacher < User
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
   def teach
-    KNOWLEDGE.sample
+    KNOWLEDGE.sample #.sample will return 1 random string from the above array 
   end
 
 end
