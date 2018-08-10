70066cfe45023d814137fbdd5a2e9f97250bdec7
Tom  Cho <tc.thomascho@gmail.com>
2018-08-10 16:50:43 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+require_relative './user.rb'
+
+class Student < User
+  attr_reader :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(knowledge)
+    self.knowledge << knowledge
+  end
+
 
 end
