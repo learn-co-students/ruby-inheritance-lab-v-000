c6fd8c40ec15d5a7a8f26b420e5df6dae5afe14e
Jae Yong Kim <kim900180@gmail.com>
2018-08-13 17:11:42 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+class Student < User
 
+  attr_accessor :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(string)
+    @knowledge << string
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
