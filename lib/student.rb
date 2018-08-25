48cc50d69ac3970d2b2a1735d8d08a0667eef6ac
sharonrieger <sharonarieger@gmail.com>
2018-08-24 20:17:31 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+# require "user.rb"
 
+class Student < User
+
+  attr_accessor :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(knowledge)
+    @knowledge << knowledge
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
