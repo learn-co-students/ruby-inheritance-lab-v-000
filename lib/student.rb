636d0b0df580feeb275bbcccdbb5a9d252b31993
Laura Darlak <laura@polyporedesign.com>
2018-09-07 15:47:16 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require_relative "./user.rb"
+
+class Student < User
+  
+  def initialize
+    @knowledge = []
+  end
+  
+  def learn(piece_of_knowledge)
+    @knowledge << piece_of_knowledge
+  end
+  
+  def knowledge
+    @knowledge
+  end
 
 end
