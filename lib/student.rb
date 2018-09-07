c793140c8ef3ff01d6ef44c052c374a1b80c4d44
Bailey Sullivan <bailey.rose.sullivan@gmail.com>
2018-09-07 00:37:48 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
 class Student < User
+   attr_accessor :knowledge
+  
+  def initialize
+    @knowledge = []
+  end 
+  
+  def learn(fact)
+    @fact = fact 
+    @knowledge << fact 
+  end 
+  
+  def self.knowledge 
+    @knowledge
+  end 
   
 end
--- lib/user.rb
+++ lib/user.rb
@@ -1,11 +1,6 @@
 class User 
-  
-  attr_accessor :first_name, :last_name 
-  
-  def initialize(first_name, last_name)
-    @first_name = first_name
-    @last_name = last_name 
-  end 
-  
+
+attr_accessor :first_name, :last_name 
+
   
 end 
