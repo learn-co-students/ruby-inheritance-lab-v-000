674a1aa4bd793dce6c4d4dfa04e313976dfd3c05
Steven Crouse <sdcrouse91@gmail.com>
2018-09-10 05:00:41 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,12 @@
-class Student 
-
+class Student < User
+  attr_reader :knowledge 
+  
+  def initialize
+    @knowledge = [] 
+  end
+  
+  def learn(knowledge_string)
+    self.knowledge << knowledge_string
+  end
+  
 end
