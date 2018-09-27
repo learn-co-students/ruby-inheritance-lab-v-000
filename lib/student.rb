c52702fe9907b9c6aa3160b06fa7d8b8300e8452
Sherry Martin <sm674@cornell.edu>
2018-09-27 21:15:33 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+  attr_accessor :knowledge
 
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(value)
+    @knowledge << value 
+  end
+  
+  def knowledge
+    @knowledge
+  end
 end
