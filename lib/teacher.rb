2758d78c6a0f4ff53a7ebe15dc958909934e5b5c
Samantha brake <sammy.brake@gmail.com>
2018-09-29 04:54:16 +0000

    Done.

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,4 +1,4 @@
-class Teacher
+class Teacher < User 
 
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
