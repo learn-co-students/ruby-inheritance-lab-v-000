870eb18ac19f2e3aa3097336b48cd62dc0c36901
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-10-02 01:15:41 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+require 'pry'
+
+class Student < User
+
+  def initialize
+    @knowledge = Array.new
+  end
+
+  def learn(new_knowelege)
+    @knowledge << new_knowelege
+  end
+
+  def knowledge
+    @knowledge
+  end
+
 
 end
