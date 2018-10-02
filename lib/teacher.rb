f3da8ddc2fd1ac18c39132ee47115c4bb1316fc8
Benjamin Jones <bljones36@gmail.com>
2018-10-02 18:14:48 +0000

    Done.

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,6 +1,9 @@
-class Teacher
+class Teacher < User
 
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
-  
+  def teach
+    max = KNOWLEDGE.size - 1
+    KNOWLEDGE[Random.rand(max)]
+  end
 end
