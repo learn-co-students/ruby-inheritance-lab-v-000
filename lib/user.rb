85e4fa7529124f8a15177bc60caa1bd23cde3bc3
Brian Birdwell <bbirdwell@gmail.com>
2018-10-07 04:31:45 +0000

    Done.

---
+++ lib/user.rb
@@ -0,0 +1,6 @@
+class User
+  attr_accessor :first_name, :last_name
+  
+end
+
+# Define the `User` class such that a user can have a first and last name. You'll need both a setter and a getter for first and last name.
