8c1b0c0ccdb6ba2ca517f890414d899a0043da91
Susan Meyers <smmeyers24@gmail.com>
2018-10-15 05:02:58 +0000

    Done.

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,6 +1,24 @@
-class Teacher
+class Teacher < User
 
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
+  
+  def teach
+   random_KNOWLEDGE = KNOWLEDGE.sample
+  end
+
+# use Array#sample:
+
+# @countries = ["Canada", "Denmark", "Germany", "Isle of Man", "Namibia", "Qatar", "South Africa",  "United Kingdom","United States"]
+# random_country = @countries.sample
+# # => "Canada"
+
+
+
+
+
+
+    
+  
 
   
 end
