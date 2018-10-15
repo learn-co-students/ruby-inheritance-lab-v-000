1ddc74d267975fd63546c1ddb26dd7d4c4f804ea
Stef Claus <stef.claus@gmail.com>
2018-10-15 12:09:22 +0000

    Done.

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,6 +1,11 @@
-class Teacher
+require_relative "./user.rb"
 
-  KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
-
+class Teacher < User 
+  
+  
+  def teach   
+  knowledge = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
+    knowledge.sample 
+  end 
   
 end
