c1ba88e11ba74e3f404eacf6ae1ae456d0bfe7d8
Christina Williams <christinaxtwilliams@gmail.com>
2018-10-16 15:32:25 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+class Student < User
+	
 
-end
+	  def initialize
+	    @knowledge = []
+	  end
+	
+
+	  def learn(string)
+	    @knowledge << string
+	  end
+	
+
+	  def knowledge
+	    @knowledge
+	  end
+	end
