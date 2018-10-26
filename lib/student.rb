2cb74106df1f3cfcb2f6aef542b3e7b7379ce8c8
Vince Andrew Pimentel <contact@vincep.me>
2018-10-26 03:42:45 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+require_relative './user.rb'
 
+class Student < User
+
+  def initialize
+    @knowledge = Array.new
+  end
+
+  def learn(str)
+    @knowledge << str
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
