3a221dd46a61ad99f23a2d2942f81c128056e753
Ryan Brady <ryanbrady369@gmail.com>
2018-11-07 20:47:14 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(to_learn)
+    @knowledge << to_learn
+  end
+
+  def knowledge
+    @knowledge
+  end
 
 end
--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,6 +1,10 @@
-class Teacher
+class Teacher < User
 
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
-  
+  def teach
+    #str = 1 + rand(KNOWLEDGE.length)
+    KNOWLEDGE[1 + rand(KNOWLEDGE.length)]
+  end
+
 end
