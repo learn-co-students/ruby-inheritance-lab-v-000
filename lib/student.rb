5b49010cac5a279f8e0913d037eedf7b07777b26
Andrew Trent <andrewtrentdesign@gmail.com>
2018-11-14 00:45:01 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require_relative "./user.rb"
+
+class Student < User
+
+  def initialize
+    @knowledge = []
+  end 
+
+  def learn(knowledge)
+    @knowledge << knowledge 
+  end
+
+  def knowledge 
+    @knowledge.dup
+  end
 
 end
