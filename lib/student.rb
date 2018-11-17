25661baa32ebfdd6faad5804cca948521bbc9f49
Paul Fell-Cinoman <swellpf@gmail.com>
2018-11-17 17:50:25 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(nugget)
+    @knowledge << nugget
+  end
+  
+  def knowledge
+    @knowledge
+  end
 
 end
