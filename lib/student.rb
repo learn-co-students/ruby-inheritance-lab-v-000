490a80287da4b602bb55dd7343f3b3346468f713
Paula McKinley <pmknyc@optimum.net>
2018-12-21 01:29:28 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
 
-end
+attr_accessor :knowledge
+
+  def initialize
+    @knowledge = []
+  #  @first_name = first_name
+  #  @last_name = last_name
+  end
+
+  def learn(string)
+    @knowledge << string
+  end
+
+end #class end
