0a776e7d5de7c5640722620b4170ded248749c23
David Charles <ratlover03@gmail.com>
2019-01-31 03:05:06 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+class Student < User
+  def initialize
+    @knowledge = []
+  end
 
+    def learn(item_learned)
+    @knowledge << item_learned
+  end
+
+    def knowledge
+    @knowledge
+  end
 end
