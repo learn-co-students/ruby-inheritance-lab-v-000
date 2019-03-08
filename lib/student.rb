3c232be33ac41564f7d02e426478c25e2abdd2f2
Niusha H <niusha.hadziomerovic@gmail.com>
2019-03-08 04:04:24 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+class Student < User
+attr_accessor :knowledge
+
+  def initialize(knowledge = [])
+    @knowledge = knowledge
+  end 
+
+  def learn(string)
+    @knowledge << string 
+  end 
+  
+  def knowledge
+    @knowledge
+  end 
 
 end
