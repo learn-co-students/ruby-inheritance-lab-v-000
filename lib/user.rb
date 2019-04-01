39cd1e96fd31c5817e37ee1303b60a99f147db6d
Gigi M <mmgamao1@gmail.com>
2019-04-01 16:44:06 +0000

    Done.

---
+++ lib/user.rb
@@ -0,0 +1,15 @@
+#1  
+class User  
+  attr_accessor :first_name, :last_name
+end  
+  
+  
+  
+  
+  
+  # def initialize(first_name, last_name)
+  #   @first_name = first_name
+  #   @last_name = last_name
+  # end
+  
+  
