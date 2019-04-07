c8a6a793974a15ff20a158c57a91fa516c68f38e
Toni-Ty <antonique2479@yahoo.com>
2019-04-07 02:59:19 +0000

    Done.

--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,30 @@
 class User
 
-attr_accessor :first, :last
+attr_accessor :first_name, :last_name
+
+def name
+  @first_name = first_name
+  @last_name = last_name
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
