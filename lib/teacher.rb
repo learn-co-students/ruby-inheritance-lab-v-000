c572410caff65375ecdd686caf6e15d7b7b8f431
Christian V <viray.christianj@gmail.com>
2019-04-11 23:15:31 +0000

    Done.

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,5 +1,6 @@
 class Teacher < User
 
+
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
 
@@ -15,5 +16,6 @@ class Teacher < User
   def teach
     KNOWLEDGE.sample
   end 
+  
   
 end
--- lib/user.rb
+++ lib/user.rb
@@ -3,9 +3,11 @@ class User
   
   attr_accessor :first_name, :last_name
   
+  
   def initialize
     @first_name
     @last_name
   end 
+  
   
 end 
