ba8c6fabe4571952792062d9917864af2551f3f8
Tam Vu <vutam_a@hotmail.com>
2019-07-04 16:27:30 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
-
+class Student < User 
+  
+  attr_accessor :knowledge
+  
+  def initialize
+    @knowledge = []
+  end 
+  
+  def learn(learned)
+    @knowledge << learned
+  end 
+  
+  def knowledge
+    @knowledge
+  end 
 end
