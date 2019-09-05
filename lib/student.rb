1f47853ff94b1a80c5bc70335f329829f141b54e
Paris Dolfman <parisdolfman@gmail.com>
2019-09-05 18:37:12 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -2,4 +2,16 @@ require_relative "./user.rb"
 
 class Student < User
 
+def initialize
+  @knowledge = []
+end
+
+def learn(string)
+  @knowledge << string
+end
+
+def knowledge
+  @knowledge
+end
+
 end
