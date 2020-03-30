b31323ede0713e88c5ebd78ab8a49f9621ce7ac7
Luis Herrera <herrera.lrh@gmail.com>
2020-03-30 17:06:49 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,16 +1,12 @@
 class Student < User
   attr_accessor :knowledge
-
+  
   def initialize
     @knowledge = []
   end
 
   def learn(string)
-    @knowledge << string
-  end
-
-  def knowledge
-    @knowledge
+    self.knowledge << string
   end
 
 end
