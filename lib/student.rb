e51bd799bed24e83e423aa5a73e03bfe70ca36db
Sai Petluri <spetluri.1@gmail.com>
2021-03-21 01:14:46 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+require_relative "./user.rb"
+class Student < User
+  
+  def initialize
+    
+    @knowledge = []
+    
+  end
+  
+  def learn(knowledge_string)
+    @knowledge << knowledge_string
+  end
+  
+  def knowledge
+    @knowledge
+  end
 
 end
